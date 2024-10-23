8a0c3416942da1cf35527c6a2ea636a5b62ea4e0
Nayanika Goje <126023861+gojenaya@users.noreply.github.com>
2024-10-23 13:06:54 +0530

    Completed Map Widget

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,13 @@
 import { useState } from "react";
-
+import MapZap from "./Widgets/MapZap/MapZap";
 import "./App.css";
-import MapboxMapComp from "./Widgets/MemoryMap/MapboxMapComp";
-import CardScreen from "./Widgets/MemoryMap/CardScreen";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <div className="flex justify-center  overflow-hidden ">
-      <MapboxMapComp />
+    <div className=" ">
+      <MapZap />
     </div>
   );
 }
